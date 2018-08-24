664547f99f9a601aa2d71ba09bf3cb05c7f7fc01
Chuck Robinson <gdgkctest@users.noreply.github.com>
2018-08-24 16:27:39 -0500

    Phase 1 : Add a Movie Screening Widget with TextView

--- app/src/main/java/com/chuck/android/widgetbasics/MovieTimesActivity.java
+++ app/src/main/java/com/chuck/android/widgetbasics/MovieTimesActivity.java
@@ -1,8 +1,12 @@
 package com.chuck.android.widgetbasics;
 
+import android.app.Application;
+import android.appwidget.AppWidgetManager;
+import android.content.ComponentName;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.RemoteViews;
 import android.widget.TextView;
 
 import com.chuck.android.widgetbasics.model.MovieShowing;
@@ -23,10 +27,30 @@ public class MovieTimesActivity extends AppCompatActivity {
         TextView movieTimes = findViewById(R.id.movie_times);
         //Randomize movie showtimes by using random odd/even times
         Random r = new Random();
+        //Problem no append method for remoteviews so appending one string to send to it
+        String widgetMovieTimes = "";
         List<MovieShowing> currentMovieTheaterTimes = MovieShowing.generateMovieTimes(currentTheaterName, r.nextBoolean());
         for (MovieShowing currentMovie : currentMovieTheaterTimes) {
             movieTimes.append(currentMovie.getMovieTitle() + "\n");
+            widgetMovieTimes = widgetMovieTimes + currentMovie.getMovieTitle() + "\n";
             movieTimes.append(currentMovie.getMovieTimes() + "\n\n");
+            widgetMovieTimes = widgetMovieTimes + currentMovie.getMovieTimes() + "\n\n";
         }
+
+        AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(this);
+        RemoteViews remoteViews = new RemoteViews(getPackageName(), R.layout.new_app_widget);
+        remoteViews.setTextViewText(R.id.appwidget_movie_theater, currentTheaterName);
+        remoteViews.setTextViewText(R.id.appwidget_movie_times, widgetMovieTimes);
+        ComponentName thisWidget = new ComponentName(this, NewAppWidget.class);
+        appWidgetManager.updateAppWidget(thisWidget, remoteViews);
+
+    }
+    //Kept this in to show how not to do it, call it multiple times erases the existing view
+    public void updateMovieWidget(String widgetText, int textBoxID){
+        AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(this);
+        RemoteViews remoteViews = new RemoteViews(getPackageName(), R.layout.new_app_widget);
+        remoteViews.setTextViewText(textBoxID, widgetText);
+        ComponentName thisWidget = new ComponentName(this, NewAppWidget.class);
+        appWidgetManager.updateAppWidget(thisWidget, remoteViews);
     }
 }
